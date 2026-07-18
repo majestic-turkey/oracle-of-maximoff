7f595f624a954253485284303324e85def2af385
A Majestic Turkey <ragingjockstrap@gmail.com>
2026-07-18 03:29:49 -0400

    Implement html-to-text parsing to create raw text from markup-derived HTML

--- packages/core/src/corpora/parseMarkup.ts
+++ packages/core/src/corpora/parseMarkup.ts
@@ -1,8 +1,12 @@
 import Parser from 'wikiparser-node'
+import { convert } from 'html-to-text'
 import readline from 'node:readline'
 import fs from 'node:fs'
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import type { Doc } from '../types.ts'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const rlInterface = readline.createInterface({
     input: fs.createReadStream(path.join(__dirname, '../../../../data/corpus/simplewiki_markup.jsonl'), { encoding: 'utf-8' }),
@@ -12,10 +16,17 @@ const rlInterface = readline.createInterface({
 // Parse each body field, creating an AST representation of the markup
 for await (const line of rlInterface) {
     const doc: Doc = JSON.parse(line)
-    const bodyAST = Parser.parse(doc.body)
+    const body = doc.body
 
     // Parse the AST into raw text
-    const rawText = parseAST(bodyAST)
+    const options = {
+        selectors: [
+            { selector: 'a', options: { ignoreHref: true } },
+            { selector: 'img', format: 'skip' },
+            { selector: 'figure', format: 'skip' },
+        ],
+    }
+    const rawText = convert(Parser.toHtml(body), options)
     doc.body = rawText
 
     // Write the updated document back to the file
@@ -25,17 +36,4 @@ for await (const line of rlInterface) {
         docString + '\n',
         { encoding: 'utf-8' }
     )
-}
-
-function parseAST(ast: any): string {
-    for (const node of ast) {
-        if (node.type === 'text') {
-            return node.data
-        } else if (node.type === 'quote') {
-            return node.text.data
-        } else if (node.type === 'link') {
-            return node.linktarget.text.data
-        }
-    }
-    return ''
 }
