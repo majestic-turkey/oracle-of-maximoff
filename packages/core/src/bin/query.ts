44e760951045d3933371ca0412438ea7c75d161b
A Majestic Turkey <ragingjockstrap@gmail.com>
2026-07-22 04:16:51 -0400

    Add scoring and return a map (docid -> score)

--- packages/core/src/bin/query.ts
+++ packages/core/src/bin/query.ts
@@ -2,7 +2,7 @@ import { parseArgs } from 'node:util'
 import analyze from '../tools/nlp.ts'
 import db from '../db/db.js'
 
-const { positionals } = parseArgs({
+const { positionals, values } = parseArgs({
     args: process.argv.slice(2),
     allowPositionals: true,
     options: {
@@ -11,17 +11,39 @@ const { positionals } = parseArgs({
     },
 })
 
+const k1 = parseFloat(values.k1)
+const b = parseFloat(values.b)
+
 const query = positionals.join(' ')
 
 const tokenizedQuery = analyze(query)
 
 const queryStatement = db.prepare(`
-    select t.id, p.frequency, p.doc_id from terms t
-    left join postings p on t.id = p.term_id
+    select t.id, p.frequency, p.doc_id, d.token_count from terms t
+    join postings p on t.id = p.term_id
+    join documents d on p.doc_id = d.id
     where t.term = ?
     `)
 
+const queryScores = new Map<number, number>()
+
+const docCount = db.prepare('select count(*) as count from documents').get().count
+const avgDocLength = db.prepare('select avg(token_count) as avgdl from documents').get().avgdl
+
 for (const { token } of tokenizedQuery) {
     const rows = queryStatement.all(token)
-    console.log(`Results for token "${token}":`, rows)
+    const df = rows.length
+    const idf = Math.log(1 + (docCount - df + 0.5) / (df + 0.5))
+    
+    for (const row of rows) {
+        const docLength = row.token_count
+        const tf = row.frequency
+        const termScore = idf * ((tf * (k1 + 1)) / (tf + k1 * (1 - b + b * (docLength / avgDocLength))))
+
+        queryScores.set(row.doc_id, (queryScores.get(row.doc_id) ?? 0) + termScore)
+    }
 }
+
+queryScores.forEach((score, docId) => {
+    console.log(`Doc ID: ${docId}, Score: ${score}`)
+})
