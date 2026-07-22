e5671e17fece4f5f988a1698fb6236e558f2e33f
A Majestic Turkey <ragingjockstrap@gmail.com>
2026-07-22 03:18:30 -0400

    Run DB query per term in the query

--- packages/core/src/bin/query.ts
+++ packages/core/src/bin/query.ts
@@ -1,4 +1,6 @@
 import { parseArgs } from 'node:util'
+import analyze from '../tools/nlp.ts'
+import db from '../db/db.js'
 
 const { positionals } = parseArgs({
     args: process.argv.slice(2),
@@ -11,4 +13,15 @@ const { positionals } = parseArgs({
 
 const query = positionals.join(' ')
 
-console.log(`Query: ${query}`)
+const tokenizedQuery = analyze(query)
+
+const queryStatement = db.prepare(`
+    select t.id, p.frequency, p.doc_id from terms t
+    left join postings p on t.id = p.term_id
+    where t.term = ?
+    `)
+
+for (const { token } of tokenizedQuery) {
+    const rows = queryStatement.all(token)
+    console.log(`Results for token "${token}":`, rows)
+}
